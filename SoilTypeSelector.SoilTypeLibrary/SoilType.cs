6191a4502303f445b1220179a1f987d83ba9e567
Lukasz Maslanka <lukasz.maslanka@gmail.com>
2012-09-24 16:37:40 -0400

    Organized the porject a bit, still more refectoring needs to be done. Also not all cases are covered in unit tests.

--- SoilTypeSelector.SoilTypeLibrary/SoilType.cs
+++ SoilTypeSelector.SoilTypeLibrary/SoilType.cs
@@ -1,14 +1,55 @@
 ﻿namespace SoilTypeSelector.SoilTypeLibrary
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using SoilTypeSelector.PolygonLibrary;
 
     public class SoilType
     {
         public string Name { get; set; }
         public TernaryCoordinate[] Coordinates { get; set; }
+
+        private readonly PolygonTools polyTools;
+
+        public string GetSoilType(TernaryCoordinate coord)
+        {
+            var samplePoint = ConvertTernaryToCartesianCoordinates(coord);
+            
+            foreach (var soilType in SoilTypeValues.SoilTypes)
+            {
+                var poly = soilType.GetPolygon();
+                if (polyTools.InsidePolygon(poly, poly.Length, samplePoint) == 0)
+                {
+                    return soilType.Name;
+                }
+            }
+
+            return "N/A";
+        }
+
+        private Point[] GetPolygon()
+        {
+            int index = 0;
+            Point[] polygon = new Point[Coordinates.Length];
+            foreach (var coordinate in Coordinates)
+            {
+                polygon[index] = ConvertTernaryToCartesianCoordinates(coordinate);
+                index++;
+            }
+
+            return polygon;
+        }
+
+        private Point ConvertTernaryToCartesianCoordinates(TernaryCoordinate coord)
+        {
+            double xCoord = (1.0D / 2.0D) * ((2.0D * coord.Silt + coord.Clay) / (coord.Sand + coord.Silt + coord.Clay));
+            double yCoord = (Math.Sqrt(3.0D) / 2.0D) * (coord.Clay / (coord.Sand + coord.Silt + coord.Clay));
+
+            return new Point() { x = xCoord, y = yCoord };
+        }
+
+        public SoilType()
+        {
+            this.polyTools = new PolygonTools();
+        }
     }
 }
---
+++ SoilTypeSelector.Tests/SoilTypeSelectorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoilTypeSelector.SoilTypeLibrary;
+
+namespace SoilTypeSelector.Tests
+{
+    [TestClass]
+    public class SoilTypeSelectorTests
+    {
+        private TernaryCoordinate ClaySamplePoint = new TernaryCoordinate { Sand = 15, Silt = 15, Clay = 70 };
+        private TernaryCoordinate SandyClaySamplePoint = new TernaryCoordinate { Sand = 50, Silt = 5, Clay = 45 };
+
+        private SoilType soil;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            soil = new SoilType();
+        }
+
+        [TestMethod]
+        public void GetType_ProvidePoint_GetBackSoilTypeAsClay()
+        {
+            Assert.IsTrue(soil.GetSoilType(ClaySamplePoint) == "Clay");
+        }
+
+        [TestMethod]
+        public void GetType_ProvidePoint_GetBackSoilTypeAsSandyClay()
+        {
+            Assert.IsTrue(soil.GetSoilType(SandyClaySamplePoint) == "Sandy Clay");
+        }
+    }
+}
